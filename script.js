4e0f6e7253250d5e0e08cd9d2f7573e601d68c40
TolgaErtugrul <tolgaertugrul@gmail.com>
2026-02-05 13:27:28 +0300

    Ilk JavaScript dosyasi ve selamlama fonksiyonu eklendi

---
+++ script.js
@@ -0,0 +1,10 @@
+// 1. Değişken Tanımlama (Bellekte yer açma)
+let ogrenciAdi = "Geleceğin Developer'ı"; 
+
+// 2. Konsola Yazdırma (Yazılımcıların gizli günlüğü)
+console.log("Sistem yüklendi, merhaba " + ogrenciAdi);
+
+// 3. İlk Fonksiyonumuz (Bir işi yapan kod bloğu)
+function selamlamaVer() {
+    alert("Merhaba! Fullstack yolculuğunda bugün JavaScript öğreniyoruz.");
+}
