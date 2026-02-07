22c07e04ee8bc82a37f014a80068c40d8a2c490f
TolgaErtugrul <tolgaertugrul@gmail.com>
2026-02-07 16:37:49 +0300

    DOM Manipulasyonu ile dinamik selamlama eklendi

--- script.js
+++ script.js
@@ -6,5 +6,22 @@ console.log("Sistem yüklendi, merhaba " + ogrenciAdi);
 
 // 3. İlk Fonksiyonumuz (Bir işi yapan kod bloğu)
 function selamlamaVer() {
-    alert("Merhaba! Fullstack yolculuğunda bugün JavaScript öğreniyoruz.");
+    // 1. Input kutusundaki değeri yakala
+    const kullaniciIsmi = document.getElementById("isim").value;
+    
+    // 2. Yazılacak alanı yakala
+    const mesajKutusu = document.getElementById("mesajAlani");
+
+    // 3. Kontrol et: İsim yazılmış mı?
+    if (kullaniciIsmi === "") {
+        mesajKutusu.innerHTML = "Lütfen önce isminizi yazın! ✍️";
+        mesajKutusu.style.color = "red";
+    } else {
+        // 4. Ekrana dinamik mesajı yazdır
+        mesajKutusu.innerHTML = "Hoş geldin, <strong>" + kullaniciIsmi + "</strong>! Bugün harika kod yazıyorsun. 🚀";
+        mesajKutusu.style.color = "green";
+        
+        // Konsola da yazdıralım (Yazılımcı alışkanlığı)
+        console.log("Yeni kullanıcı selamlandı: " + kullaniciIsmi);
+    }
 }
